33cb635aa3d3c6570e2015f856b5b238b41e68f3
PHAN THIỆN NHÂN <phanthiennhan248@gmail.com>
2023-06-04 09:33:59 +0700

    Nhan's GUI and DTO

---
+++ GUI_HotelManagement/CustomerDetail.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace WinFormsApp2
+{
+    partial class CustomerDetail
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            panel1 = new Panel();
+            CreateBooking = new Button();
+            label11 = new Label();
+            textBox10 = new TextBox();
+            textBox9 = new TextBox();
+            textBox8 = new TextBox();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(CreateBooking);
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(textBox10);
+            panel1.Controls.Add(textBox9);
+            panel1.Controls.Add(textBox8);
+            panel1.Controls.Add(textBox7);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 426);
+            panel1.TabIndex = 7;
+            panel1.Paint += panel1_Paint;
+            // 
+            // CreateBooking
+            // 
+            CreateBooking.Location = new Point(617, 371);
+            CreateBooking.Name = "CreateBooking";
+            CreateBooking.Size = new Size(128, 29);
+            CreateBooking.TabIndex = 36;
+            CreateBooking.Text = "Create Booking";
+            CreateBooking.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            label11.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            label11.Location = new Point(292, 1);
+            label11.Name = "label11";
+            label11.Size = new Size(205, 35);
+            label11.TabIndex = 35;
+            label11.Text = "Customer Detail";
+            label11.TextAlign = ContentAlignment.TopCenter;
+            label11.Click += label11_Click;
+            // 
+            // textBox10
+            // 
+            textBox10.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox10.Location = new Point(575, 303);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(170, 27);
+            textBox10.TabIndex = 34;
+            // 
+            // textBox9
+            // 
+            textBox9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox9.Location = new Point(544, 234);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(201, 27);
+            textBox9.TabIndex = 33;
+            // 
+            // textBox8
+            // 
+            textBox8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox8.Location = new Point(501, 172);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(244, 27);
+            textBox8.TabIndex = 32;
+            // 
+            // textBox7
+            // 
+            textBox7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox7.Location = new Point(501, 108);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(244, 27);
+            textBox7.TabIndex = 31;
+            // 
+            // textBox6
+            // 
+            textBox6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox6.Location = new Point(501, 43);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(244, 27);
+            textBox6.TabIndex = 30;
+            // 
+            // textBox5
+            // 
+            textBox5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox5.Location = new Point(121, 303);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(244, 27);
+            textBox5.TabIndex = 29;
+            // 
+            // textBox4
+            // 
+            textBox4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox4.Location = new Point(121, 234);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(244, 27);
+            textBox4.TabIndex = 28;
+            // 
+            // textBox3
+            // 
+            textBox3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox3.Location = new Point(165, 172);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(200, 27);
+            textBox3.TabIndex = 27;
+            // 
+            // textBox2
+            // 
+            textBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox2.Location = new Point(121, 108);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(244, 27);
+            textBox2.TabIndex = 26;
+            // 
+            // label10
+            // 
+            label10.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label10.AutoSize = true;
+            label10.Location = new Point(420, 310);
+            label10.Name = "label10";
+            label10.Size = new Size(132, 20);
+            label10.TabIndex = 25;
+            label10.Text = "Number of people";
+            label10.Click += label10_Click;
+            // 
+            // label9
+            // 
+            label9.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label9.AutoSize = true;
+            label9.Location = new Point(420, 241);
+            label9.Name = "label9";
+            label9.Size = new Size(94, 20);
+            label9.TabIndex = 23;
+            label9.Text = "Group Name";
+            label9.Click += label9_Click;
+            // 
+            // label8
+            // 
+            label8.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label8.AutoSize = true;
+            label8.Location = new Point(420, 179);
+            label8.Name = "label8";
+            label8.Size = new Size(67, 20);
+            label8.TabIndex = 21;
+            label8.Text = "Duration";
+            // 
+            // label7
+            // 
+            label7.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label7.AutoSize = true;
+            label7.Location = new Point(420, 115);
+            label7.Name = "label7";
+            label7.Size = new Size(76, 20);
+            label7.TabIndex = 19;
+            label7.Text = "Check-out";
+            // 
+            // label6
+            // 
+            label6.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label6.AutoSize = true;
+            label6.Location = new Point(420, 54);
+            label6.Name = "label6";
+            label6.Size = new Size(66, 20);
+            label6.TabIndex = 17;
+            label6.Text = "Check-in";
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.Location = new Point(27, 303);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Email";
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Location = new Point(27, 241);
+            label4.Name = "label4";
+            label4.Size = new Size(30, 20);
+            label4.TabIndex = 13;
+            label4.Text = "Fax";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 179);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 20);
+            label3.TabIndex = 11;
+            label3.Text = "Phone Number";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 115);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 20);
+            label2.TabIndex = 9;
+            label2.Text = "Address";
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(121, 47);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(244, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(76, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Full Name";
+            label1.Click += label1_Click_1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(517, 371);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 37;
+            button1.Text = "Save";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // CustomerDetail
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "CustomerDetail";
+            Text = "Customer Detail";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private Panel panel1;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label1;
+        private Label label10;
+        private TextBox textBox10;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label11;
+        private Button CreateBooking;
+        private Button button1;
+    }
+}
---
+++ GUI_HotelManagement/RoomList.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace GUI_HotelManagement
+{
+    partial class RoomList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Black", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(277, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(247, 46);
+            label1.TabIndex = 1;
+            label1.Text = "Customer List";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(575, 106);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Fillter";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(624, 101);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(164, 28);
+            comboBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 147);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(776, 292);
+            dataGridView1.TabIndex = 6;
+            // 
+            // RoomList
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Name = "RoomList";
+            Text = "RoomList";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private DataGridView dataGridView1;
+    }
+}
