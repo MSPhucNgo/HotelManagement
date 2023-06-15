38647d70ccc73942baa87d960f018c4278625925
Dương ITUS <duongdeep@gmail.com>
2023-06-15 12:13:59 +0700

    DUONG

--- Script/DUONG.sql
+++ Script/DUONG.sql
@@ -2,16 +2,19 @@
 select * from ITEM
 
 -- Liệt kê danh sách vật dụng theo mã phòng
-----CREATE FUNCTION fu_List_Of_Items_Room 
-----( @room_id INT )
-----RETURNS VARCHAR(50)
-----AS
-----BEGIN
-----DECLARE @room_id VARCHAR(50);
-----select distinct I.NAME
-----from ITEM I join ITEM_ROOM IR on I.ID_ITEM = IR.ID_ITEM
-----where IR.ID_ROOM = @room_id;
-----RETURN @room_id;
-----END;
+CREATE FUNCTION fu_List_Of_Items_Room 
+( @room_id INT )
+RETURNS VARCHAR(50)
+AS
+BEGIN
+DECLARE @room_id VARCHAR(50);
+
+select distinct I.NAME, IR.AMOUNT
+from ITEM I join ITEM_ROOM IR on I.ID_ITEM = IR.ID_ITEM
+where IR.ID_ROOM = '1005'
+
+@room_id;
+RETURN @room_id;
+END;
 
 -- Liệt kê tất cả vật dụng
