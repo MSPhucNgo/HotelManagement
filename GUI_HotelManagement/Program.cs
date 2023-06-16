81395f3a472fb3d979cd28be32d81aa8ce35da20
vuongtanphat139 <73147318+vuongtanphat139@users.noreply.github.com>
2023-06-16 16:08:28 +0700

    fix bug

--- GUI_HotelManagement/Program.cs
+++ GUI_HotelManagement/Program.cs
@@ -1,24 +1,23 @@
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace GUI_HotelManagement
 {
-    internal static class Program
+    internal class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new login_GUI());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new temp());
+            //Application.Run(new List_Of_Items());
         }
     }
 }
